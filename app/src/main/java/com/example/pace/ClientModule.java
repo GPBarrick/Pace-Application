3ea6ef5a93355b18fe0570936e059ad1a29a4978
GPBarrick <124000118+GPBarrick@users.noreply.github.com>
2023-11-05 12:17:39 -0500

    Updated Tasks
    Added instructions for functions responsible for calculations.

--- app/src/main/java/com/example/pace/ClientModule.java
+++ app/src/main/java/com/example/pace/ClientModule.java
@@ -1,34 +1,42 @@
 package com.example.pace;
 public class ClientModule {
-private String DateText;
-private int ImageResource;
+    private String DateText;
+    private int ImageResource;
 
-//constructor for variable.
-public void ClientModule(String DateText, int ImageResource){
-    this.DateText = DateText;
-    this.ImageResource = ImageResource;
-}
+    //constructor for variable.
+    public ClientModule(String DateText, int ImageResource){
+        this.DateText = DateText;
+        this.ImageResource = ImageResource;
+    }
 
-//getters
+    //getters
     public String getDateText() {
         return DateText;
     }
-
     public int getImageResource() {
         return ImageResource;
     }
 
     //Setters
-
     public void setDateText(String dateText) {
         DateText = dateText;
     }
-
-
     public void setImageResource(int imageResource) {
         ImageResource = imageResource;
     }
 }
+
+/* 11/5/2023
+*   Create private members with getters and setters to use when calculating the information below.
+*
+*   Create calculation functions to calculate:
+* - Amount of money spent on gas when driving a certain distance while considering, gas price, and MPG
+* - Averages for routes, distance, money spent on gas, income
+*
+* - The averages will be done by creating a function with parameters of (ArrayList<ClientModule>)
+* and using that parameter to view the list of current client modules we will be using to calculate averages.
+* For example, I will call this function in MainActivity.java and pass in an ArrayList of ClientModules and
+* the function within this class is responsible for calculating the averages based on the list provided */
 
 /* 11/5/2023 Create the class members that will represent the statistics we will be manipulating
 * Must have getters and setters, as well as constructor initialization. Check the data you are using
