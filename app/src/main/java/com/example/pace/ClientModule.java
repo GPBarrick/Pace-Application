516435fbe4dca4461f24eb9d57b52a33e187609e
GPBarrick <124000118+GPBarrick@users.noreply.github.com>
2023-11-05 11:16:12 -0500

    ClientModule Instructions
    Created a class and gave instructions that describe how to add functionality.

---
+++ app/src/main/java/com/example/pace/ClientModule.java
@@ -0,0 +1,8 @@
+package com.example.pace;
+public class ClientModule {
+
+}
+
+/* 11/5/2023 Create the class members that will represent the statistics we will be manipulating
+* Must have getters and setters, as well as constructor initialization. Check the data you are using
+* by comparing it to null as a safety feature. */
