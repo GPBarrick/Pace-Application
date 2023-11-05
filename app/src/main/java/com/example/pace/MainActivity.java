2cb16dea201ebad4601eafc2331373c31dd70241
GPBarrick <124000118+GPBarrick@users.noreply.github.com>
2023-11-05 11:10:07 -0500

    Foundation Commit
    This is the initial commit of the base project we will build upon.

---
+++ app/src/main/java/com/example/pace/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.pace;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
