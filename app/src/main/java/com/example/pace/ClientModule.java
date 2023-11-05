3d69b939f285af5d44aee51d6524d95c23023def
sgerda <135041922+sgerda@users.noreply.github.com>
2023-11-05 12:09:57 -0500

    Tool bar/Client Module. & Getters/setter for Client Module

--- app/src/main/java/com/example/pace/ClientModule.java
+++ app/src/main/java/com/example/pace/ClientModule.java
@@ -1,6 +1,33 @@
 package com.example.pace;
 public class ClientModule {
+private String DateText;
+private int ImageResource;
 
+//constructor for variable.
+public void ClientModule(String DateText, int ImageResource){
+    this.DateText = DateText;
+    this.ImageResource = ImageResource;
+}
+
+//getters
+    public String getDateText() {
+        return DateText;
+    }
+
+    public int getImageResource() {
+        return ImageResource;
+    }
+
+    //Setters
+
+    public void setDateText(String dateText) {
+        DateText = dateText;
+    }
+
+
+    public void setImageResource(int imageResource) {
+        ImageResource = imageResource;
+    }
 }
 
 /* 11/5/2023 Create the class members that will represent the statistics we will be manipulating
