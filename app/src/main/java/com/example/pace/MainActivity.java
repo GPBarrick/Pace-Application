0baf0de7bd41e70151ea12876908bfb4229e9deb
GPBarrick <124000118+GPBarrick@users.noreply.github.com>
2023-11-21 15:52:07 -0500

    build
    bones build

--- app/src/main/java/com/example/pace/MainActivity.java
+++ app/src/main/java/com/example/pace/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pace;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/pace/fragmentinputlayouts/InputMpgFragment.java
@@ -0,0 +1,7 @@
+package com.example.pace.fragmentinputlayouts;
+
+import androidx.fragment.app.Fragment;
+
+public class InputMpgFragment extends Fragment {
+
+}
