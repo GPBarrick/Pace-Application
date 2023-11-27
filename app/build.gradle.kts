fe2c2d5f95947b5f228edb90c53db20bcf3140ea
sgerda <135041922+sgerda@users.noreply.github.com>
2023-11-27 13:21:40 -0500

    UnImportant commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,15 +38,16 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-<<<<<<< HEAD
+
     // Import the BoM for the Firebase platform
     implementation ("com.google.firebase:firebase-bom:32.5.0")
     implementation ("com.google.android.gms:play-services-auth:18.1.0")
 
-=======
+
     implementation("androidx.viewpager2:viewpager2:1.0.0")
->>>>>>> gavin
+
 }
