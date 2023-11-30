2f65317e93e0660712e5fa0b2b1e15a80bbfda81
sgerda <135041922+sgerda@users.noreply.github.com>
2023-11-30 15:56:25 -0500

    Revert "Merge branch 'gavin-new' into saul"
    This reverts commit 93d6f370ff72b6dea90ca8b0f38df0856a4c3338, reversing
    changes made to 8e60fc0637334177008ced83fbfe27103f3118e4.

--- app/src/main/java/com/example/pace/fragmentelements/MonthlyListFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.pace.fragmentelements;
-
-import androidx.fragment.app.Fragment;
-
-public class MonthlyListFragment extends Fragment {
-}
--- app/src/main/java/com/example/pace/fragmentelements/WeeklyListFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.pace.fragmentelements;
-
-import androidx.fragment.app.Fragment;
-
-public class WeeklyListFragment extends Fragment {
-}
